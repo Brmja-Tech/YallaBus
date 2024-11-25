1b30937f52afa46434cfe887088a2eb63c5417e8
Abdelrhman Adel <abdelrhmanadel2512>
2024-11-25 19:14:54 +0200

    register

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main(){
+import 'features/register/register_screen.dart';
+import 'core/styles/light_theme.dart';
+
+void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: const Size(440, 956),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          // You can use the library anywhere in the app even in theme
+          theme: LightTheme.theme,
+          home: child,
+        );
+      },
+      child: RegisterScreen(),
+    );
   }
 }
