487cc5d959a9abb18ac8beef47bccd3f64e8707a
Mahmoud Ahmed <103232226+MahmoudAhmed2003@users.noreply.github.com>
2024-11-25 19:22:44 +0200

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,48 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sama_bus/features/Auth/view/login_screen.dart';
+import 'package:sama_bus/source/core/styles/light_theme.dart';
 
-void main(){
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  // runApp(MyApp());
+  runApp(EasyLocalization(
+    supportedLocales: const [
+      Locale('en'),
+      Locale('ar'),
+    ],
+    saveLocale: true,
+    path: 'assets/lang',
+    startLocale: const Locale('en'),
+    // fallbackLocale: const Locale('en'),
+    child: const MyApp(),
+  ));
+
+  // runApp(LocalizationService.rootWidget(child: const MyApp()));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+        designSize: const Size(440, 956),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        // Use builder only if you need to use library outside ScreenUtilInit context
+        builder: (_, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            theme: LightTheme.theme,
+            home: const LoginScreen(),
+          );
+        });
   }
 }
