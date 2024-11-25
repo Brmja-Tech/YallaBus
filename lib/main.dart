aad4c1d64eec99a7af8069fd42f23475de3e11a7
Mahmoud Ahmed <103232226+MahmoudAhmed2003@users.noreply.github.com>
2024-11-25 19:25:40 +0200

    sdsd

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sama_bus/core/styles/light_theme.dart';
 import 'package:sama_bus/features/Auth/view/login_screen.dart';
-import 'package:sama_bus/source/core/styles/light_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
