264eff02475edd11a7d9f57f7bf398307c08d5ff
Abdelrhman Adel <abdelrhmanadel2512>
2024-11-25 19:21:04 +0200

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,25 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'features/register/register_screen.dart';
 import 'core/styles/light_theme.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
 
-void main() {
-  runApp(const MyApp());
+  // runApp(MyApp());
+  runApp(EasyLocalization(
+    supportedLocales: const [
+      Locale('en'),
+      Locale('ar'),
+    ],
+    saveLocale: true,
+    path: 'assets/lang',
+    startLocale: const Locale('en'),
+    // fallbackLocale: const Locale('en'),
+    child: const MyApp(),
+  ));
+
+  // runApp(LocalizationService.rootWidget(child: const MyApp()));
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
