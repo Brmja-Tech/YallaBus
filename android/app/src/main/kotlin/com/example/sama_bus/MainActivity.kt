8f08d5670ac0f1c235e63946dbbe0849f08ad826
Mahmoud Ahmed <103232226+MahmoudAhmed2003@users.noreply.github.com>
2024-11-25 16:16:51 +0200

    initializing the app

---
+++ android/app/src/main/kotlin/com/example/sama_bus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sama_bus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/source/core/styles/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:sama_bus/source/core/styles/color_extension.dart';
+
+
+abstract final class AppColors {
+  static final Color primaryColor = HexColor.fromHex('#1C6AE4');
+  static final Color secondaryColor = HexColor.fromHex('#323142');
+  static final Color gestureColor = HexColor.fromHex('#0061FF');
+}
